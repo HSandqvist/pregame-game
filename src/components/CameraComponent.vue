faf26cc6b84f1be9d462873a6cca73ad2efef2bd
Marie Stjernholm <marie.lindali.stjernholm@gmail.com>
2025-01-10 16:06:53 +0100

    inkludera componenterna oxå

---
+++ src/components/CameraComponent.vue
@@ -0,0 +1,245 @@
+<template>
+  <div class="camera-component-container">
+    <h1>{{ uiLabels.captureYourAvatar || "Capture your avatar" }}</h1>
+    <div class="camera-container">
+      <!-- camera view -->
+      <div class="camera-view">
+        <p v-if="!isPictureTaken">
+          <video ref="video" width="320" height="240" autoplay></video>
+        </p>
+        <p v-if="isPictureTaken">
+          <img :src="avatar" alt="User Avatar" width="320" height="240" />
+        </p>
+      </div>
+      <!-- camera buttons-->
+      <div class="camera-buttons">
+        <button @click="startCamera" :disabled="cameraState">
+          {{ uiLabels.startCamera || "Start camera" }}
+        </button>
+        <button @click="captureImage" :disabled="!cameraState">
+          {{ uiLabels.takePicture || "Take picture" }}
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//importera bild på kamera att ha som default när ingen bild har tagits
+
+export default {
+  name: "CameraComponent",
+  props: {
+    uiLabels: {},
+    disableSwitcher: {},
+    isPictureTaken: {},
+  },
+  data: function () {
+    return {
+      avatar: null, // Ensure avatar is defined in the data
+      cameraState: false,
+      stream: null, // The video stream to access the camera
+    };
+  },
+  methods: {
+    toggleSwitcher() {
+      // Emit event to parent to update disableSwitcher state
+      this.$emit("update:disableSwitcher", !this.disableSwitcher);
+    },
+    takePicture() {
+      // Emit event to parent to update isPictureTaken state
+      this.$emit("update:isPictureTaken", true);
+    },
+    // Start the camera stream
+    startCamera: function () {
+      this.$emit("update:isPictureTaken", false);
+      this.$emit("update:disableSwitcher", true);
+      //this.isPictureTaken = false;
+      this.cameraState = true;
+      //this.disableSwitcher = true;
+
+      // Stop any existing camera stream before starting a new one, make sure always turned off
+      if (this.stream) {
+        const tracks = this.stream.getTracks();
+        tracks.forEach((track) => track.stop());
+        this.stream = null;
+      }
+      // Stop the video element from using the old stream
+      if (this.$refs.video) {
+        this.$refs.video.srcObject = null;
+      }
+      //start new camera stream
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          this.stream = stream;
+          this.$refs.video.srcObject = stream;
+          console.log("Camera stream is active:", stream);
+        })
+        .catch((error) => {
+          console.error("Error accessing camera:", error);
+          alert(
+            "Unable to access the camera. Please check your camera settings."
+          );
+          this.cameraState = false; // Allow retry if error occurs
+        });
+    },
+
+    // Stop the camera stream
+    stopCamera: function () {
+      if (this.stream) {
+        console.log("stopping stream", this.stream);
+        const tracks = this.stream.getTracks();
+        tracks.forEach((track) => track.stop()); //stop all tracks
+        this.stream = null; //added
+      }
+      if (this.$refs.video) {
+        this.$refs.video.srcObject = null; // Clear the video element source
+      }
+      this.cameraState = false;
+      this.$emit("update:disableSwitcher", false);
+      //this.disableSwitcher = false;
+    },
+
+    // Capture the image from the video stream
+    captureImage: function () {
+      const video = this.$refs.video;
+
+      //this.isPictureTaken = true;
+      this.$emit("update:isPictureTaken", true);
+
+      if (video && video.videoWidth > 0 && video.videoHeight > 0) {
+        // Create a canvas to capture the image
+        const canvas = document.createElement("canvas");
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+
+        // Get the context and draw the video frame to the canvas
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        // Convert canvas to a base64 image string
+        this.avatar = canvas.toDataURL("image/png");
+
+        // Log to check the base64 image
+        console.log("Avatar captured");
+        //console.log("Captured Avatar: ", this.avatar);
+
+        this.cameraState = false; // Disable camera actions
+
+        // Stop the camera stream after capturing the image
+        this.stopCamera();
+      } else {
+        console.error("Video stream is not available.");
+      }
+
+      this.$emit("update:avatar", this.avatar);
+    },
+
+    beforeUnmount: function () {
+      this.stopCamera(); // Clean up the stream when leaving component
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Camera and camera buttons styling */
+.camera-component-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Vertically center items */
+  justify-content: center; /* Horizontally center */
+  width: 100%;
+  box-sizing: border-box;
+}
+
+.camera-component-container h1 {
+    margin-bottom: 3rem; /* Remove default margin from h1 */
+    padding: 1rem; /* Remove any padding */}
+
+.camera-container {
+  display: flex;
+  align-items: center; /* Center vertically in the container */
+  justify-content: center; /* Align to the left */
+  gap: 4rem; /* Space between camera view and the buttons */
+}
+
+.camera-view {
+  position: relative;
+  display: flex; /* Enable flexbox for centering */
+  align-items: center; /* Vertical centering */
+  justify-content: center; /* Horizontal centering */
+  border: 0.1rem solid #f01984;
+  border-radius: 50%;
+  background-color: #f01984;
+  width: 15rem;
+  height: 15rem;
+  overflow: hidden; /* Clip overflow to maintain circular shape */
+}
+
+/* Camera video style */
+video,
+img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.camera-buttons {
+  display: flex; /* Arrange buttons vertically */
+  flex-direction: column;
+  gap: 10px;
+  align-items: flex-start; /* Align buttons to the left of the container */
+}
+
+.camera-buttons button {
+  padding: 12px 20px;
+  background-color: rgb(225, 95, 150); /* Darker pink */
+  color: white;
+  border: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+  transition: all 0.2s ease;
+}
+
+.camera-buttons button:hover {
+  background-color: rgb(205, 85, 140); /* Darker hover effect */
+}
+
+button:disabled {
+  pointer-events: none; /* Prevent any mouse interactions */
+  opacity: 0.5; /* Make the button appear transparent */
+  cursor: not-allowed; /* Change the cursor to indicate the button is disabled */
+  transition: none; /* Disable any hover or transition effects */
+}
+
+/* Ensure responsive design */
+@media (max-width: 768px) {
+  .camera-picture-container {
+    flex-direction: column; /* Stack vertically on smaller screens */
+    align-items: center; /* Center items horizontally */
+  }
+
+  .camera-view {
+    width: 80%; /* Make the camera view smaller on mobile */
+    height: auto;
+  }
+
+  .camera-buttons {
+    width: 100%;
+    align-items: center; /* Center the buttons */
+  }
+
+  button {
+    width: 100%; /* Buttons take full width */
+  }
+}
+</style>
---
+++ src/components/ChooseAvatarComponent.vue
@@ -0,0 +1,167 @@
+<template>
+  <div class="choose-avatar-container">
+    <!-- Chosen Avatar Display -->
+    <div class="chosen-avatar">
+      <h1>{{ uiLabels.chooseYourAvatar || "Choose Your Avatar" }}</h1>
+
+      <div class="chosen-avatar-picture">
+        <img :src="chosenAvatar" alt="Chosen Avatar" class="avatar-image" />
+      </div>
+    </div>
+
+    <!-- Scrollable Avatar Options -->
+    <div class="avatar-options">
+      <p>{{ uiLabels.selectAnAvatar || "Avatars:" }}</p>
+
+      <div class="avatar-scroll-container">
+        <!-- Avatar Option List -->
+        <div
+          v-for="(avatarOption, index) in avatarOptions"
+          :key="index"
+          class="avatar-option"
+          @click="selectAvatar(avatarOption)"
+        >
+          <img
+            :src="avatarOption || defaultAvatar"
+            alt="Avatar Option"
+            class="avatar-option-image"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import defaultAvatar from '@/assets/img/Gorankan.png';
+import avatar1 from "/src/assets/img/Gorankan.png";
+import avatar2 from "/src/assets/img/Liankan.png";
+import avatar3 from "/src/assets/img/Plankan.png";
+import avatar4 from "/src/assets/img/Bankan.png";
+
+export default {
+  props: {
+    uiLabels: Object, // Language support for UI Labels
+    isPictureTaken: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      chosenAvatar: defaultAvatar, 
+      defaultAvatar,
+      avatarOptions: [ avatar1, avatar2, avatar3, avatar4 ],
+    };
+  },
+  methods: {
+    selectAvatar(avatar) {
+      this.chosenAvatar = avatar;
+      this.$emit("update:avatar", avatar);
+      this.$emit("update:isPictureTaken", true);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.choose-avatar-container {
+  display: flex;
+  justify-content: space-between; /* Put elements in a row */
+  align-items: flex-start; /* Align items at the top */
+  gap: 2rem;
+  width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.chosen-avatar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  flex-direction: column;
+}
+
+.chosen-avatar-picture {
+  border: 0.1rem solid #f01984;
+  border-radius: 50%;
+  background-color: #f01984;
+  width: 15rem;
+  height: 14rem;
+}
+
+.avatar-image {
+  width: 100%; /* Make the image stretch to full width */
+  height: 100%; /* Make the image stretch to full height */
+  object-fit: cover; /* Ensure the image covers the area */
+  border-radius: 50%;
+  border: none;
+}
+
+.avatar-options {
+  display: flex;
+  flex-direction: column; /* Similar to camera buttons */
+  gap: 1rem;
+  align-items: center;
+  background-color: #ffa0d0;
+  border-radius: 1rem;
+  min-width: 20%;
+}
+
+.avatar-options p {
+  font-size: 1.25rem;
+  font-weight: bold;
+  text-align: center;
+  color: #f01984;
+}
+
+.avatar-scroll-container {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  max-height: 300px;
+  overflow-y: auto; /* Enable vertical scrolling */
+}
+
+.avatar-option {
+  cursor: pointer;
+  text-align: center;
+}
+
+.avatar-option img {
+  width: 4rem;
+  height: 4rem;
+  object-fit: cover;
+  border-radius: 50%;
+  border: 0.15rem solid #f01984;
+  transition: transform 0.3s ease-in-out;
+}
+
+.avatar-option:hover img {
+  cursor: pointer;
+  transform: scale(1.04);
+}
+
+@media (max-width: 768px) {
+  .choose-avatar-container {
+    flex-direction: column; /* Stack items vertically on mobile */
+    align-items: center;
+  }
+
+  .chosen-avatar {
+    width: 80%; /* Adjust size on mobile */
+    height: auto;
+  }
+
+  .avatar-options {
+    width: 100%; /* Ensure options container takes full width on mobile */
+    align-items: center;
+  }
+
+  .avatar-option img {
+    width: 3rem;
+    height: 3rem;
+  }
+}
+</style>
